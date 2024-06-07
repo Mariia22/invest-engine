5177c382e433838fb58c5304551bb2072b2c5f25
Maria Ushakova <m.ushakova22@gmail.com>
2024-06-07 13:21:23 +0200

    fix: card props

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,8 @@ import { cards } from './data/data'
 function App() {
   return (
     <section className='container'>
-      {cards.map((card: CardProps) => (
-        <Card
-          key={card.key}
-          theme={card.theme}
-          clickable={card.clickable}
-          cardLink={card.cardLink}
-          onClick={card.onClick}
-          title={card.title}
-          titleSize={card.titleSize}
-          text={card.text}
-          imageLink={card.imageLink!}
-          alt={card.alt!}
-          width={card.width!}
-          height={card.height!}
-          style={card.style}
-          className={card.className}
-        />
-      ))}
+      {cards.map((card: CardProps) => (<Card {...card} />))}
     </section>
   )
 }
-
 export default App
